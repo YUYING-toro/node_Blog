db7be1c1c24ef73419e4faf649b7869c11c20be1
Sandra <000@gmail.com>
2021-12-29 23:05:00 -0500

    ask url/index error status

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var router = express.Router();
+//archieve show article cards
+var moment = require('moment');
+var striptags = require('striptags');
+
 var firebaseAdminDB = require("../connection/fireBase_admin"); // env connect firebase 但隱藏鑰匙; exports= firebaseAdmin.database();
-var createPath = firebaseAdminDB.ref('/categories');
+var categoriesRef = firebaseAdminDB.ref('/categories');
 var articlesRef = firebaseAdminDB.ref('/articles');
 
 /* GET users listing. */
+//抓總分類目錄
 router.get('/article/create', function(req, res) {
-  createPath.once('value').then(function(snapshot){
+  categoriesRef.once('value').then(function(snapshot){
     const categories = snapshot.val(); 
     res.render('dashboard/article', { 
       title: 'Express',
@@ -16,15 +21,15 @@ router.get('/article/create', function(req, res) {
 });
 router.get('/article/:id', function(req, res) { 
   var id = req.param("id");
-  console.log(id);
+  // console.log(id);
   let categories ={};
-  createPath.once('value').then(function(snapshot){ // 取得文章類別
+  categoriesRef.once('value').then(function(snapshot){ // 取得文章類別
     categories = snapshot.val();  //id: '-MrykWiPvwWyYp4H6yfV', name: '12', path: '12'
     //snapshot send to html
      return articlesRef.child(id).once('value'); // 用id 快照文章 再 撈取 用 once(value).then(function) 避免層層包疊 
   }).then(function(snapshot){ //快照該id 找到的文章
     const article = snapshot.val();
-    console.log(article);
+    // console.log(article);
 
     res.render('dashboard/article', { 
       title: 'Express',
@@ -45,15 +50,60 @@ router.post('/article/create', function(req, res) {
     res.redirect(`/dashboard/article/${key}`);
   })  
 })
+router.post('/article/update/:id', function(req, res) {
+  const data = req.body;
+  const id = req.param('id');
+  const updateTime = Math.floor(Date.now()/1000);
+  data.time = updateTime;
+  articlesRef.child(id).update(data).then(function(){
+    res.redirect(`/dashboard/article/${id}`);
+  })  
+})
+//顯示文章列表
+router.get('/archives', function(req, res) {    
+  //控制 "草稿" 及 "公開" 文章 > '/archives? status = public
+  const status = req.query.status || 'public';  // 77
 
-router.get('/archives', function(req, res, next) {
-  res.render('dashboard/archives', { title: 'Express' });
+  //取得目錄 與 文章串
+  let categories = {}; 
+  categoriesRef.once('value').then(function(snapshot){
+    categories = snapshot.val();  
+    // console.log(categories);
+    return articlesRef.orderByChild('time').once('value'); //由舊到新 
+  }).then(function(snapshot){
+    const articles = [];//因為要做分頁 所以改為陣列  
+    snapshot.forEach(function(snapshotChild){
+      if(snapshotChild.val().status === status){
+        articles.push(snapshotChild.val())
+      }      
+    })
+   articles.reverse() ; //array func
+    res.render('dashboard/archives', { 
+      title: 'Express',
+      articles,
+      categories,
+      striptags,
+      moment,
+      status
+    });
+  })
 });
+//delete
+router.post('/article/delete/:id', function(req, res) {
+  const id = req.param("id");
+   console.log("delete id is "+ id);
+  articlesRef.child(id).remove();
+  req.flash('info',"You have deleted successfully")
+ // res.redirect('/dashboard/categories'); //用ajax 刪除 因此用 res.send("data")傳資料 、 end()結束
+  res.send("文章已刪除");
+  res.end();
+})
+
 router.get('/categories', function(req, res, next) {
   //retrive post delete flash 我有載入套件connect-flash & express-session
   const messageFromSession = req.flash('info');  // line 46 flash('info'," You have deleted successfully.") 得字串
   //retrive create categories 
-  createPath.once('value',function(snapshot){
+  categoriesRef.once('value',function(snapshot){
     const categories = snapshot.val();  //id: '-MrykWiPvwWyYp4H6yfV', name: '12', path: '12'
     //snapshot send to html
     res.render('dashboard/categories', { 
@@ -68,11 +118,11 @@ router.get('/categories', function(req, res, next) {
 router.post('/categories/create', function(req, res) {
   // console.log(req.body);
   var data = req.body; // array
-  const categoryRef = createPath.push(); // 路徑 root/categories/亂碼 {id:亂碼, mame~, path~ } // push 會產亂碼 此亂碼可用 key取用
+  const categoryRef = categoriesRef.push(); // 路徑 root/categories/亂碼 {id:亂碼, mame~, path~ } // push 會產亂碼 此亂碼可用 key取用
   // console.log(categoryRef.key); //!!!!!!!!!!!!!!!!!!!!! 取得亂碼
   data.id = categoryRef.key ; //array add one more data
 //判斷有無此已創建此目錄名稱
-  createPath.orderByChild('path').equalTo(data.path).once('value')
+  categoriesRef.orderByChild('path').equalTo(data.path).once('value')
   .then(function(snapshot){
     if(snapshot.val() !== null){
       // 有撈取相同
@@ -93,7 +143,7 @@ router.post('/categories/delete/:id', function(req, res) {
   //crab id
   var id = req.param("id");
   // console.log("id is "+ id);
-  createPath.child(id).remove();
+  categoriesRef.child(id).remove();
   req.flash('info',"You have deleted successfully")
   res.redirect('/dashboard/categories');
 })
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var firebaseAdminDB = require("../connection/fireBase_admin"); // env connect firebase 但隱藏鑰匙
+
+//archieve show article cards
+var moment = require('moment');
+var striptags = require('striptags');
+var firebaseAdminDB = require("../connection/fireBase_admin"); // env connect firebase 但隱藏鑰匙; exports= firebaseAdmin.database();
+var categoriesRef = firebaseAdminDB.ref('/categories');
+var articlesRef = firebaseAdminDB.ref('/articles');
 
 //測試 env　有連線上
 /*const ref = firebaseAdminDB.ref('any');
@@ -8,9 +14,29 @@ ref.once('value',function(snapshot){
   console.log(snapshot.val());
 })*/
 
-/* GET home page. */
+/* GET home page. 同 dashboard/archievs */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    //取得目錄 與 文章串
+    let categories = {}; 
+    categoriesRef.once('value').then(function(snapshot){
+      categories = snapshot.val();  
+      return articlesRef.orderByChild('time').once('value'); //由舊到新 
+    }).then(function(snapshot){
+      const articles = [];//因為要做分頁 所以改為陣列  
+      snapshot.forEach(function(snapshotChild){
+        if(snapshotChild.val().status === 'public'){  //首頁僅顯示 公開文章
+          articles.push(snapshotChild.val())
+        }      
+      })
+     articles.reverse() ; //array func
+      res.render('dashboard/archives', { 
+        title: 'Express',
+        articles,
+        categories,
+        striptags,
+        moment
+      });
+    })
 });
 router.get('/post', function(req, res, next) {
   res.render('post', { title: 'Express' });
